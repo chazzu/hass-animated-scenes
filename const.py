6f9f290b3402481f31769ccb397125f8d1692ace
Dustin Ashby <dustin.ashby@beauthentic.digital>
2020-10-28 21:39:28 -0400

    initial commit

---
+++ const.py
@@ -0,0 +1,29 @@
+"""Constants for the Animated Scenes integration."""
+
+DOMAIN = "animated_scenes"
+
+CONF_EXTERNAL_SWITCHES = 'external_switches'
+
+CONF_ANIMATE_BRIGHTNESS = 'animate_brightness'
+CONF_ANIMATE_COLOR = 'animate_color'
+CONF_BRIGHTNESS = 'brightness'
+CONF_CHANGE_AMOUNT = 'change_amount'
+CONF_CHANGE_FREQUENCY = 'change_frequency'
+CONF_COLORS = 'colors'
+CONF_COLOR = 'color'
+CONF_COLOR_TEMP = 'color_temp'
+CONF_COLOR_HS = 'hs_color'
+CONF_COLOR_RGB = 'rgb_color'
+CONF_COLOR_XY = 'xy_color'
+CONF_COLOR_TYPE = 'color_type'
+CONF_IGNORE_OFF = 'ignore_off'
+CONF_LIGHTS = 'lights'
+CONF_NAME = 'name'
+CONF_ONE_CHANGE_PER_TICK = 'one_change_per_tick'
+CONF_PLATFORM = 'platform'
+CONF_RESTORE = 'restore'
+CONF_TRANSITION = 'transition'
+CONF_WEIGHT = 'weight'
+
+DEFAULT_MIN_BRIGHT = 70
+DEFAULT_MAX_BRIGHT = 100
